4fc1eded0c75dfb364e0842439ef8b220942af9f
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-07-22 15:36:40 +0900

    Edit MainActivity.java (SingleChoiceItems) Commit

--- app/src/main/java/kr/hs/emirim/chaehyeon/ex14_15/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/ex14_15/MainActivity.java
@@ -7,40 +7,80 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.dialog.InsetDialogOnTouchListener;
+
 public class MainActivity extends AppCompatActivity {
+    String[] fruitsArr = {"수박","참외","복숭아"};
+    boolean[] checkArr = {true, false, true};
+    int[] imgsArr = {R.drawable.img01, R.drawable.img02, R.drawable.img03};
+    Button btnFruits;
+    ImageView imgV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnDialog = findViewById(R.id.btn_dialog);
+        btnFruits = findViewById(R.id.btn_fruits);
         btnDialog.setOnClickListener(btnListener);
+        btnFruits.setOnClickListener(btnListener);
+        imgV = findViewById(R.id.imgv);
     }
-
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            /*AlertDialog.Builder dialog = new AlertDialog.Builder(getApplicationContext());
-            dialog.setTitle(R.string.dialog_title);
-            dialog.setMessage(R.string.dialog_message);
-            dialog.setIcon(R.drawable.warning);
-            dialog.setPositiveButton(R.string.btn_positive, btnPositiveListener);
-            dialog.show();*/
-            new AlertDialog.Builder(getApplicationContext())
-            .setTitle(R.string.dialog_title)
-            .setMessage(R.string.dialog_message)
-            .setIcon(R.drawable.warning)
-            .setPositiveButton(R.string.btn_positive, btnPositiveListener)
-            .show();
+            switch (v.getId()){
+                case R.id.btn_dialog:
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle(R.string.dialog_title)
+                            .setMessage(R.string.dialog_message)
+                            .setIcon(R.drawable.watermelon)
+                            .setPositiveButton(R.string.btn_positive, btnPositiveListener)
+                            .show();
+                    break;
+                case R.id.btn_fruits:
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle(R.string.btn_fruits)
+                            .setSingleChoiceItems(fruitsArr, 0,  fruitsItemListener)
+                            .setIcon(R.drawable.watermelon)
+                            .setPositiveButton("닫기", null)
+                            .show();
+                    break;
+            }
+        }
+    };
+
+    DialogInterface.OnMultiChoiceClickListener fruitsMultiItemListener = new DialogInterface.OnMultiChoiceClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+            btnFruits.setText(fruitsArr[which]);
+            imgV.setImageResource(imgsArr[which]);
+        }
+    };
+
+    DialogInterface.OnClickListener fruitsItemListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            btnFruits.setText(fruitsArr[which]);
+        }
+    };
+
+    DialogInterface.OnClickListener FruitsItemListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            btnFruits.setText(fruitsArr[which]);
+            imgV.setImageResource(imgsArr[which]);
         }
     };
 
     DialogInterface.OnClickListener btnPositiveListener = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialog, int which) {
-            Toast.makeText(MainActivity.this, "대화상자의 확인 버튼을 클릭하셨어요~", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "태화상자 확인 버튼을 클릭함", Toast.LENGTH_SHORT).show();
         }
     };
+
 }
